53fb7596649fc975238a376b5468429f64b12642
onegoodthing <siddhantdhyani10@gmail.com>
2025-07-07 00:10:34 +0100

    Adding Chrome options for maximizing the window and Launching in Particular Dimension.

--- src/main/java/selenium/sample/NewWindowHandle.java
+++ src/main/java/selenium/sample/NewWindowHandle.java
@@ -3,20 +3,28 @@ package selenium.sample;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 
 import java.util.Set;
 
 public class NewWindowHandle {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+/*Adding Chrome options for maximizing the window and Launching in Particular Dimension.
+* */
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://academy.naveenautomationlabs.com/");
-        driver.manage().window().maximize();
-        /**
+
+        /*
          * CLick on Demo Site link for new window
          */
 
         try {
+            Assert.assertTrue(driver.getPageSource().contains("404"));
             String pageTitleMain = driver.getTitle();
             System.out.println("Main Window page = " + pageTitleMain);
             driver.findElement(By.xpath("//a[normalize-space()='Demo Site']")).click();
@@ -33,13 +41,15 @@ public class NewWindowHandle {
 
             }
             driver.switchTo().window(mainWindow);
-            /**
+            /*
              * Performing actions in the main window
              */
             driver.findElement(By.xpath("//a[@class='edit']")).click();
             System.out.println("New Window title = " + driver.getTitle());
         } catch (Exception e) {
-            e.printStackTrace();
+               e.getStackTrace();
+            throw new RuntimeException();
+
         } finally {
             driver.close();
         }
